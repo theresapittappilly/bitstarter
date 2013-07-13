8c6deb1acc97820c9e12d04667ca92db8c6eea48
Theresa <theresa.pittappilly@live.com>
2013-07-13 06:48:06 +0000

    Test output of fs.readFileSync2

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //  response.send('Test');
-    response.send(fs.readFileSync(index.html));
+    response.send(fs.readFileSync(~/hw3/bitstarter/index.html));
 });
 
 var port = process.env.PORT || 5000;
